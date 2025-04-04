fa268f672c9d5797c817e86c4d0f906b6cc38a9f
einmar <einar.westman@gmail.com>
2025-04-04 22:33:11 +0200

    tanky mac tankface

---
+++ health.gd
@@ -0,0 +1,20 @@
+extends Node2D
+class_name Health
+
+signal health_changed 
+signal health_depleted
+
+@export var max_health: int = 100
+var health: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	health = max_health
+
+func take_damage(amount):
+	health -= amount
+	health = max(0, health)
+	emit_signal("health_changed", health)
+	
+	if health == 0:
+		emit_signal("health_depleted")
--- scenes/car/car.gd
+++ scenes/car/car.gd
@@ -3,8 +3,8 @@ extends RigidBody2D
 
 
 @export var player: int = 0
-@export var steering_force: float = 200000
-@export var acceleration: float = 2000
+@export var steering_force: float = 10000
+@export var acceleration: float = 4000
 @export var max_speed: float = 400
 
 var steering: float = 0
@@ -16,24 +16,29 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	
 	#print(get_last_motion()) stuped
 	#velocity = get_last_motion()
 	var steering_input: float 
 	var acceleration_input: float
 	if Input.is_joy_known(0):
 		steering_input = Input.get_joy_axis(player,JOY_AXIS_LEFT_X)
-		acceleration_input = Input.get_joy_axis(player,JOY_AXIS_LEFT_Y)
+		acceleration_input = -Input.get_joy_axis(player,JOY_AXIS_LEFT_Y)
 	else:
 		steering_input = Input.get_axis("player1_left","player1_right")
-		acceleration_input = Input.get_axis("player1_up","player1_down")
+		acceleration_input = Input.get_axis("player1_down","player1_up")
 	#steering = steering_input * deg_to_rad(steering)
 	#rotate(steering_input * steering_force * delta)
 	apply_torque(steering_input * steering_force * delta)
 	#speed += acceleration
-	apply_central_force(transform.y * (acceleration_input * acceleration * delta))
+	apply_central_force(transform.x * (acceleration_input * acceleration * delta))
 	#velocity += transform.y * (acceleration * 40 * delta)
 	#move_and_slide()
 	#smoothstep(speed,max_speed, )
 
 func attacked(damage:int):
 	print("Damage taken: ", damage)
+
+
+func _on_health_health_depleted() -> void:
+	print("you died!")
