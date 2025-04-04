8101f3992f9f4ddb78fe8fc67c1a983abd93367b
einmar <einar.westman@gmail.com>
2025-04-04 18:40:49 +0200

    Car steering

--- Scenes/Player/Car/car.gd
+++ Scenes/Player/Car/car.gd
@@ -1,8 +1,19 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var player: int = 0
+@export var steering_force: float = 5
+@export var max_speed: float = 400
+
+var steering: float = 0
+var speed: float = 0
 
 func _physics_process(delta: float) -> void:
-	var steering = Input.get_joy_axis(player,JOY_AXIS_LEFT_X)
+	var steering_input = Input.get_joy_axis(player,JOY_AXIS_LEFT_X)
 	var acceleration = Input.get_joy_axis(player,JOY_AXIS_LEFT_Y)
-	print(steering)
+	#steering = steering_input * deg_to_rad(steering)
+	
+	rotate(steering_input * steering_force * delta)
+	speed += acceleration
+	velocity = transform.y * (speed)
+	move_and_slide()
+	#smoothstep(speed,max_speed, )
