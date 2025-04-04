4e613aae52b415d1ac8ec0971943265e4fbe1def
vozdok <60823071+vozdok@users.noreply.github.com>
2025-04-04 21:23:41 +0200

    avlusade koden

--- systems/game_manager.gd
+++ systems/game_manager.gd
@@ -7,7 +7,7 @@ var player: RigidBody2D
 func add(key: String, value):
 	references[key] = value
 
-func remove(key: String):s
+func remove(key: String):
 	if references.has(key):
 		references.erase(key)
 		return true
