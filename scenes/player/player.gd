f60e31b0db252f41fe7eb9a277db58cee9b07357
einmar <einar.westman@gmail.com>
2025-04-05 21:42:08 +0200

    swim movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -19,23 +19,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	if _active_station == null:
-		# Handle jump.
 		
-		if Input.is_action_just_pressed("player{i}_jump".format({"i":player_index})) and is_on_floor():
+		if Input.is_action_just_pressed(input_key("jump")) and is_on_floor() and not in_water:
 			velocity.y = JUMP_VELOCITY
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis(
-			"player{i}_left".format({"i":player_index}), 
-			"player{i}_right".format({"i":player_index}),
-		)
+		
+		var direction := Input.get_axis(input_key("left"), input_key("right"))
 		if direction:
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		var vertical_direction = Input.get_axis(input_key("up"),input_key("down"))
+		if vertical_direction and in_water:
+			velocity.y = vertical_direction * SPEED
 	animate()
-	
+	if in_water:
+		velocity = velocity / 2.0
 	move_and_slide()
 
 func animate() -> void:
@@ -46,7 +45,7 @@ func animate() -> void:
 
 	var idle = is_on_floor() and is_zero_approx(velocity.x)
 	var swim_idle = in_water and is_zero_approx(velocity.x)
-	var jumping = Input.is_action_just_pressed("player{i}_jump".format({"i":player_index})) and is_on_floor()
+	var jumping = Input.is_action_just_pressed(input_key("jump")) and is_on_floor()
 	
 	if interataction_stopped:
 		if sprite_animated.animation == "interaction_back_stop":
