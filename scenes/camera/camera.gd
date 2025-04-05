755e1792cd6f67c2d0f72c06e14a4f51323d74ce
vozdok <60823071+vozdok@users.noreply.github.com>
2025-04-05 12:25:19 +0200

    added camera reference

--- scenes/camera/camera.gd
+++ scenes/camera/camera.gd
@@ -3,6 +3,9 @@ extends Camera2D
 @export var target: Node2D
 @export var follow_strength: float = 10 
 
+func _ready() -> void:
+	GameManager.main_camera = self
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = lerp(position, target.position, follow_strength/100.0)
