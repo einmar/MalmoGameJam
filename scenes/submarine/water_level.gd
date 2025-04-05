4a56c930d8d5f0b76d8d3d5156a303af8c873db4
einmar <einar.westman@gmail.com>
2025-04-05 21:42:18 +0200

    rotate water

--- scenes/submarine/water_level.gd
+++ scenes/submarine/water_level.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	global_rotation = sprite_rotation
 
 
 func _on_submarine_water_level_changed(water_level: float) -> void:
