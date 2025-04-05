be0a06176a20f54518e23dc5efe632db15121fa2
einmar <einar.westman@gmail.com>
2025-04-05 11:17:26 +0200

    submarine buoyancy

--- scenes/submarine/submarine.gd
+++ scenes/submarine/submarine.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 class_name Submarine
 
 
-var buoyancy: float = 6.0
+var buoyancy: float = 970.0
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -11,8 +11,8 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += Vector2(get_gravity().x, get_gravity().y - buoyancy) * delta
+	if not is_on_floor():
+		velocity += Vector2(get_gravity().x, get_gravity().y - buoyancy) * delta
 
 
 
