ed511baaadade36a431258b7082ec8ac7dd87d0e
einmar <einar.westman@gmail.com>
2025-04-05 12:37:34 +0200

    fixed camera

--- scenes/camera/camera.gd
+++ scenes/camera/camera.gd
@@ -4,5 +4,5 @@ extends Camera2D
 @export var follow_strength: float = 10 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position = lerp(position, target.position, follow_strength/100.0)
+#func _process(delta: float) -> void:
+#	position = lerp(position, target.position, follow_strength/100.0)
--- scenes/submarine/submarine.gd
+++ scenes/submarine/submarine.gd
@@ -4,6 +4,9 @@ class_name Submarine
 
 
 var buoyancy: float = 980.0
+var speed: float = 70.0
+var player_index: int = 1
+var is_steering: bool = false
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -15,7 +18,19 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += Vector2(get_gravity().x, get_gravity().y - buoyancy) * delta
-
-
+	if Input.is_action_just_pressed("player{i}_primary".format({"i":player_index})):
+		is_steering = not is_steering
+	if is_steering:
+		var direction:= Vector2(Input.get_axis(
+				"player{i}_left".format({"i":player_index}), 
+				"player{i}_right".format({"i":player_index}),
+			),
+			Input.get_axis(
+				"player{i}_up".format({"i":player_index}), 
+				"player{i}_down".format({"i":player_index}),
+			),
+		)
+		
+		velocity += direction * speed * delta
 
 	move_and_slide()
