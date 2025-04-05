b3cafe1fbde64e5fbd733fb682b31e5f9d11f256
Tim Henriksson <tim.henriksson1@inter.ikea.com>
2025-04-05 14:48:41 +0200

    gun

---
+++ scenes/gun/gun.gd
@@ -0,0 +1,25 @@
+extends Node2D
+class_name Gun
+
+@export var shot_cooldown: float = 0.5 # Cooldown time in seconds
+var last_shot : float = 0.0
+func _ready() -> void:
+    # Called when the node enters the scene tree for the first time.
+    pass
+
+func _process(delta: float) -> void:
+    # Called every frame. 'delta' is the elapsed time since the previous frame.
+    pass
+
+func rotate_gun(rotation: float) -> void:
+    # Rotate the gun by the specified amount
+    rotation_degrees += rotation
+    # Clamp the rotation to a certain range if needed
+    rotation_degrees = clamp(rotation_degrees, -45, 45)
+    # Update the gun's rotation
+    rotation_degrees = rotation_degrees
+
+func shoot() -> void:
+    var time_now = Time.get_unix_time_from_system()
+    if time_now - last_shot > shot_cooldown:
+        last_shot = time_now
--- scenes/stations/gun_station.gd
+++ scenes/stations/gun_station.gd
@@ -1,14 +1,21 @@
 ﻿extends BaseStation
 
-@export var _gun: Node2D
+var _gun_rotation_speed: float = 180.0 # degrees per second
+
+@export var _guns: Array[Gun] = []
 func _ready() -> void:
 	pass # Replace with function body.
 
 func control_station(delta) -> void:
-	if Input.is_joy_known(0):
-		pass
-	else:
-		var axis = Input.get_axis(_player.input_key("left"), _player.input_key("right"));
-		print(axis)
-	
+	var axis = Input.get_axis(_player.input_key("left"), _player.input_key("right"));
+
+	if axis != 0:
+		for gun in _guns:
+			gun.rotate_gun(-axis * _gun_rotation_speed * delta)
+
+
+	for gun in _guns:
+		if Input.is_action_pressed(_player.input_key("action")):
+			gun.shoot()
+
 	super(delta)
