ffcd19ec0a81f83465268b59138d24aca5884aa9
einmar <einar.westman@gmail.com>
2025-04-05 11:07:42 +0200

    submarine

---
+++ scenes/submarine/submarine.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var buoyancy: float = 6.0
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+	#	velocity += Vector2(get_gravity().x, get_gravity().y - buoyancy) * delta
+
+
+
+	move_and_slide()
