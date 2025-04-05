b43cd4c0ffe0a30ce29c9fc0ffc0d77627eab5ce
einmar <einar.westman@gmail.com>
2025-04-05 13:45:13 +0200

    use instead of primary

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,45 +9,46 @@ const JUMP_VELOCITY = -300.0
 
 var _active_station: BaseStation = null
 func _physics_process(delta: float) -> void:
-    # Add the gravity.
-    if not is_on_floor():
-        velocity += get_gravity() * delta
-    
-    if _active_station == null:
-        # Handle jump.
-        if Input.is_action_just_pressed("player{i}_jump".format({"i":player_index})) and is_on_floor():
-            velocity.y = JUMP_VELOCITY
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if _active_station == null:
+		# Handle jump.
+		
+		if Input.is_action_just_pressed("player{i}_jump".format({"i":player_index})) and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-        # Get the input direction and handle the movement/deceleration.
-        # As good practice, you should replace UI actions with custom gameplay actions.
-        var direction := Input.get_axis(
-            "player{i}_left".format({"i":player_index}), 
-            "player{i}_right".format({"i":player_index}),
-        )
-        if direction:
-            velocity.x = direction * SPEED
-        else:
-            velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis(
+			"player{i}_left".format({"i":player_index}), 
+			"player{i}_right".format({"i":player_index}),
+		)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-    move_and_slide()
-    
+	move_and_slide()
+	
 func _process(delta: float) -> void: 
-    var closest_station = check_for_stations() 
-    if closest_station == null: 
-        return
-    
-    if Input.is_action_just_pressed(input_key("use")):
-        if closest_station.activate(self):
-            _active_station = closest_station;
-    
-    
+	var closest_station = check_for_stations() 
+	if closest_station == null: 
+		return
+	
+	if Input.is_action_just_pressed(input_key("use")):
+		if closest_station.activate(self):
+			_active_station = closest_station;
+	
+	
 func input_key(key) -> String:
-    return "player%s_%s" % [player_index, key]
-    
+	return "player%s_%s" % [player_index, key]
+	
 func check_for_stations() -> BaseStation:
-    var sub = find_parent("Submarine")
-    return sub.get_station(position, 1000.0)
+	var sub = find_parent("Submarine")
+	return sub.get_station(position, 1000.0)
 
 func leave_station() -> void:
-    _active_station._player = null
-    _active_station = null
+	_active_station._player = null
+	_active_station = null
--- scenes/submarine/submarine.gd
+++ scenes/submarine/submarine.gd
@@ -17,19 +17,20 @@ const JUMP_VELOCITY = -400.0
 var stations: Array[BaseStation] = []
 func _ready() -> void:
 	GameManager.submarine = self
-    for child in get_children():
-        if child is BaseStation:
-            stations.append(child)
-            
-    print(stations)
+	for child in get_children():
+		if child is BaseStation:
+			stations.append(child)
+			
+	print(stations)
 
 func _physics_process(delta: float) -> void:
 	timer += delta
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += Vector2(get_gravity().x, get_gravity().y - buoyancy) * delta
-	if Input.is_action_just_pressed("player{i}_primary".format({"i":player_index})):
+	if Input.is_action_just_pressed("player{i}_use".format({"i":player_index})):
 		is_steering = not is_steering
+
 	if is_steering:
 		var direction:= Vector2(Input.get_axis(
 				"player{i}_left".format({"i":player_index}), 
@@ -45,18 +46,18 @@ func _physics_process(delta: float) -> void:
 	rotation = deg_to_rad(velocity.x / tilt_damping) + sin(timer * turbulence)/tilt_damping
 
 
-    move_and_slide()
-    
+	move_and_slide()
+	
 func get_station(pos: Vector2, radius: float) -> BaseStation:
-    
-    var closest_station = null
-    var d: float = INF
-    for station in stations:
-        var dist: float = pos.distance_to(station.transform.get_origin())
-        if dist < radius and dist < d:
-            d = dist
-            closest_station = station
-            
-    return closest_station
+	
+	var closest_station = null
+	var d: float = INF
+	for station in stations:
+		var dist: float = pos.distance_to(station.transform.get_origin())
+		if dist < radius and dist < d:
+			d = dist
+			closest_station = station
+			
+	return closest_station
 
 	
