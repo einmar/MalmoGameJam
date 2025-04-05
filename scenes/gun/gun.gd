aafc7eb1cc0eeffe3dddeb99b19de891efcb7c5a
vozdok <60823071+vozdok@users.noreply.github.com>
2025-04-05 21:32:57 +0200

    avlusning lights

--- scenes/gun/gun.gd
+++ scenes/gun/gun.gd
@@ -7,26 +7,26 @@ class_name Gun
 var exit : Node2D
 var last_shot : float = 0.0
 func _ready() -> void:
-    exit = find_child("exit")
+	exit = find_child("exit")
 
 func _process(delta: float) -> void:
-    # Called every frame. 'delta' is the elapsed time since the previous frame.
-    pass
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass
 
 func rotate_gun(rotation: float) -> void:
-    # Rotate the gun by the specified amount
-    rotation_degrees += rotation
-    # Clamp the rotation to a certain range if needed
-    rotation_degrees = clamp(rotation_degrees, -45, 45)
-    # Update the gun's rotation
-    rotation_degrees = rotation_degrees
+	# Rotate the gun by the specified amount
+	rotation_degrees += rotation
+	# Clamp the rotation to a certain range if needed
+	rotation_degrees = clamp(rotation_degrees, -45, 45)
+	# Update the gun's rotation
+	rotation_degrees = rotation_degrees
 
 func shoot() -> void:
-    var time_now = Time.get_unix_time_from_system()
-    if time_now - last_shot > shot_cooldown:
-        last_shot = time_now
+	var time_now = Time.get_unix_time_from_system()
+	if time_now - last_shot > shot_cooldown:
+		last_shot = time_now
 
-        var bullet = bullet_prefab.instantiate()
-        bullet.transform.origin = exit.global_position
-        bullet.rotation = rotation
-        get_tree().current_scene.add_child(bullet)
+		var bullet = bullet_prefab.instantiate()
+		bullet.transform.origin = exit.global_position
+		bullet.rotation = rotation
+		get_tree().current_scene.add_child(bullet)
