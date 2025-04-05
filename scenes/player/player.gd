7f211ff4b23b73c82e1450098037150c2ae3c01c
einmar <einar.westman@gmail.com>
2025-04-05 10:45:54 +0200

    player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name Player
 
+@export var player_index: int = 1
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -12,12 +14,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player{i}_jump".format({"i":player_index})) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis(
+		"player{i}_left".format({"i":player_index}), 
+		"player{i}_right".format({"i":player_index}),
+	)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
--- scenes/sandbox/tile_map_layer.gd
+++ scenes/sandbox/tile_map_layer.gd
@@ -1,6 +1,6 @@
 extends TileMapLayer
 
-
+var timer:= 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate(delta/10.0)
+	timer += delta
+	rotation = sin(timer/1.0) * 0.3
