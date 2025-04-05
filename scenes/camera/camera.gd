891c429bf2b3928736c8604dfe405459cc8d72f0
einmar <einar.westman@gmail.com>
2025-04-05 12:01:48 +0200

    camera

---
+++ scenes/camera/camera.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+@export var target: Node2D
+@export var follow_strength: float = 10 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position = lerp(position, target.position, follow_strength/100.0)
+	print(position, target.position)
