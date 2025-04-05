c6429b8a1b5c9451f9d43562f21acc9a3db640fc
Tim Henriksson <tim.henriksson1@inter.ikea.com>
2025-04-05 18:44:18 +0200

    pew pew

---
+++ scenes/gun/bullet.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# Speed of the bullet
+@export var speed: float = 400.0
+@export var damage: int = 10
+
+var velocity: Vector2 = Vector2.ZERO
+
+func _ready():
+    velocity = (-Vector2.UP).rotated(rotation) * speed
+    print(rotation)
+
+func _process(delta: float):
+    position += velocity * delta
+
+func _on_body_entered(body: Node) -> void:
+    if body is BasicEnemy:
+        body.health.take_damage(damage)  # Deal damage to the enemy
+        queue_free()  # Remove the bullet after hitting an enemy
+    else:
+        queue_free()  # Remove the bullet if it hits anything else
+
+    visible = false
--- scenes/submarine/water_level.gd
+++ scenes/submarine/water_level.gd
@@ -1,21 +1,15 @@
 extends Sprite2D
 
-var position_empty:float = 160.0
-var position_full_delta:float = -110.0
-var current_level: float = 0.0
-var sprite_rotation: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position.y = position_empty
-	sprite_rotation = global_rotation
+    scale = Vector2(0,1)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_rotation = sprite_rotation
+    pass
 
 
 func _on_submarine_water_level_changed(water_level: float) -> void:
-	var pos = position_empty + position_full_delta * water_level
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", Vector2(0,pos), 0.5)
-	
+    scale = Vector2(water_level,1)
