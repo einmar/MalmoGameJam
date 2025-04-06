ec8dc70091c79564c1a649c4a9a4f1b30845c2c6
vozdok <60823071+vozdok@users.noreply.github.com>
2025-04-06 09:45:03 +0200

    wip first level

--- scenes/stations/light_station.gd
+++ scenes/stations/light_station.gd
@@ -2,7 +2,7 @@ extends BaseStation
 
 @export var _lights: Array[Light] = []
 @export var light_energy_on: float = 4.0
-@export var light_energy_off: float = 1.0
+@export var light_energy_off: float = 0.5
 
 var _light_rotation_speed: float = 180.0 # degrees per second
 
--- scenes/turrets/bullet.gd
+++ scenes/turrets/bullet.gd
@@ -7,17 +7,17 @@ extends Node2D
 var velocity: Vector2 = Vector2.ZERO
 
 func _ready():
-    velocity = (-Vector2.UP).rotated(rotation) * speed
-    print(rotation)
+	velocity = (-Vector2.UP).rotated(rotation) * speed
+	print(rotation)
 
 func _process(delta: float):
-    position += velocity * delta
+	position += velocity * delta
 
 func _on_body_entered(body: Node) -> void:
-    if body is BasicEnemy:
-        body.health.take_damage(damage)  # Deal damage to the enemy
-        queue_free()  # Remove the bullet after hitting an enemy
-    else:
-        queue_free()  # Remove the bullet if it hits anything else
+	if body is BasicEnemy:
+		body.health.take_damage(damage)  # Deal damage to the enemy
+		queue_free()  # Remove the bullet after hitting an enemy
+	else:
+		queue_free()  # Remove the bullet if it hits anything else
 
-    visible = false
+	visible = false
