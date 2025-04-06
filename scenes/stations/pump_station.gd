306c78bde7dc779aca317eb82deae2cc23b35b75
einmar <einar.westman@gmail.com>
2025-04-06 13:06:07 +0200

    fixed submarine collision

--- scenes/stations/pump_station.gd
+++ scenes/stations/pump_station.gd
@@ -4,7 +4,6 @@ extends BaseStation
 
 
 func control_station(delta) -> void:
-	print("using pumpstation")
 	if Input.is_action_pressed(_player.input_key("action")):
 		GameManager.submarine.health.take_damage(-pump_rate * delta)
 
