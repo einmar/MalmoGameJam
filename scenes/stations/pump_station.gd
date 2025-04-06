72747162f9c1ee0978e72f1a807d00e99a4881ce
einmar <einar.westman@gmail.com>
2025-04-06 12:04:57 +0200

    better pump rate

--- scenes/stations/pump_station.gd
+++ scenes/stations/pump_station.gd
@@ -1,8 +1,11 @@
 extends BaseStation
 
+@export var pump_rate: float = 20.0
+
+
 func control_station(delta) -> void:
 	print("using pumpstation")
 	if Input.is_action_pressed(_player.input_key("action")):
-		GameManager.submarine.health.take_damage(-10.0 * delta)
+		GameManager.submarine.health.take_damage(-pump_rate * delta)
 
 	super(delta)
