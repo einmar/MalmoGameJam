15b16956f4a2e20a8e381e9ad2a6453cb45e3ce6
Alriuda <fredrik.valentin.nilsson@gmail.com>
2025-04-07 21:37:19 +0200

    Art Update

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -3,13 +3,13 @@ extends Control
 @export var level_scene: PackedScene
 
 func _on_button_pressed() -> void:
-	var tree: SceneTree = get_tree()
-	tree.change_scene_to_packed(level_scene)
+    var tree: SceneTree = get_tree()
+    tree.change_scene_to_packed(level_scene)
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("player1_use"):
-		_on_button_pressed()
+    if Input.is_action_just_pressed("player1_use"):
+        _on_button_pressed()
 
 
 func _on_option_button_item_selected(index: int) -> void:
-	GameManager.number_of_players = index + 1
+    GameManager.number_of_players = index + 1
