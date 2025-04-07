4dc8d02561c8859a50032486042f7ff511c332f0
einmar <einar.westman@gmail.com>
2025-04-07 20:07:09 +0200

    removed old level setup call. Avlusning!

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -5,12 +5,6 @@ extends Control
 func _on_button_pressed() -> void:
 	var tree: SceneTree = get_tree()
 	tree.change_scene_to_packed(level_scene)
-	tree.root.child_entered_tree.connect(
-	(func (scene: Node2D) -> void:
-		GameManager.call_deferred("level_setup")
-	),
-	CONNECT_ONE_SHOT
-	)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("player1_use"):
